776302068224b2c78ba1ae68bd2f704379f6b4f0
jihaojie <123881775@qq.com>
2018-03-06 19:10:06 +0800

    Update 01_rotatearray.py

--- 01_rotatearray.py
+++ 01_rotatearray.py
@@ -10,8 +10,6 @@ class Solution:
             return 0
 
         for i in range(len(rotateArray)-1):
-            print(i)
-            #print(rotateArray[i],rotateArray[i+1])
             if rotateArray[i] < rotateArray[i+1]:
                 print(rotateArray[i])
                 rotateArray[i], rotateArray[i+1] = rotateArray[i + 1], rotateArray[i]
